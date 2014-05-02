6f7d850f9e728443f938a783a56ab5faf064bf51
Matthew Ripley <mattew.ripley@gmail.com>
2014-05-01 22:39:51 -0600

    Add view skeleton

---
+++ src/com/twofoureight/twentyfortyeight/TwentyFortyEightView.java
@@ -0,0 +1,18 @@
+package com.twofoureight.twentyfortyeight;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class TwentyFortyEightView extends View {
+
+	public TwentyFortyEightView(Context context) {
+		super(context);
+
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		
+	}
+}
